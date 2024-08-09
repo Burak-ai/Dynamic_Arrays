3e5d3a79a76abfa9e4d58576eef045703afed415
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-08-09 23:17:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         
-        DynamicArray dynamicArray = new DynamicArray(5);
+        DynamicArray dynamicArray = new DynamicArray();
         
         dynamicArray.add("A");
         dynamicArray.add("B");
         dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
 
-        dynamicArray.insert(0, "x");
+
         dynamicArray.delete("A");
+        dynamicArray.insert(0, "x");
         System.out.println(dynamicArray.search("C"));
 
         System.out.println(dynamicArray);
