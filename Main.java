fc1f40d30829b806fb7f548926aeabe34a4b139e
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-08-09 22:35:30 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package DynamicArrays;
+public class Main {
+    
+
+    public static void main(String[] args) {
+        
+        DynamicArray dynamicArray = new DynamicArray(5);
+        
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+
+        System.out.println(dynamicArray);
+        System.out.println("size: " + dynamicArray.size);
+        System.out.println("capacity: " + dynamicArray.capacity);
+        System.out.println("empty: " + dynamicArray.isEmpty());
+    }
+}
